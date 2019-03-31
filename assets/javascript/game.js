60bda213ce91bbe75907485793608c5feb0d3e22
mtvalencia <mtv411@yahoo.com>
2019-03-31 15:37:08 -0600

    Working version of project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,53 @@
-    // Creates an array that lists out all of the city options.
-    var cityChoices = ['SAN FRANCISCO', 'SAN JOSE', 'MIAMI', 'NEW YORK'];
-    var userGuesses = [];
+    // Creates an array that lists out all of the options.
+    var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-    // Creating variables to hold the number of wins, guesses, and remaining guesses. The wins start at 0 and rguesses start at 6.
+    // Creating variables to hold the number of wins, losses, and guesses left.
     var wins = 0;
-    var guesses = 0;
-    var rGuesses = 6;
+    var losses = 0;
+    var guessesLeft = 10;
+    var userChoice = [];
 
     // Create variables that hold references to the places in the HTML where we want to display things.
-    var directionsText = document.getElementById("directions-text");
-    var userGuessesText = document.getElementById("userGuesses-text");
-    var rGuessesText = document.getElementById("rGuesses-text");
     var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var guessesLeftText = document.getElementById("guessesLeft-text");
+    var userChoiceText = document.getElementById("userchoice-text");
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
-
       // Determines which key was pressed.
       var userGuess = event.key;
 
-      // userGuesses.push(userGuess); 
-      console.log(userGuess);
-      userGuesses.push(userGuess); 
-      console.log(userGuesses);
-
       // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = cityChoices[Math.floor(Math.random() * cityChoices.length)];
-      console.log("Computer chose " + computerGuess);
+      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-      // Split computer city choice into characters.
-      var chars = computerGuess.split('');  
-      var s = computerGuess;
-      for (var i = 0; i < s.length; i++) {
-          console.log(s.charAt(i));
-      }
+      //Game over when guessesLeft is 0.
+      if(guessesLeft === 0){
+        alert("The game is over. Thanks for playing!");
+        alert("Press F5 to play again");
+      } 
 
-
-      //Display characters like _ _ _ _.
-
-
-
-      // Reworked our code from last step to use "else if" instead of lots of if statements.
-
-
+      //Add user guess to the userChoice array
+      if ((userGuess.search(/[a-z]/g) === 0) && (guessesLeft != -1)) {
+        userChoice.push(userGuess);
+        guessesLeft--;
+      } 
 
       // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+      if  ((userGuess.search(/[a-z]/g) === 0) && (guessesLeft != -1)) {
 
-
-
-        // Hide the directions
-        // directionsText.textContent = "";
-
-
-
-        // Display the user and computer guesses, and wins/losses/ties.
-
-        // rGuessesTex.textContent = "Remaining choices: ";
-        // winsText.textContent = "Wins: " + wins;
-        // userGuessesText.textContent = "You chose: " + userGuesses;
-
-    
-  };
+        if (userGuess === computerGuess) {
+          wins++;
+        } else {
+          losses++;
+        }
+        console.log("I chose letter: " + userGuess);
+        console.log("The cpu chose letter: " + computerGuess);
+        // // Display the user and computer guesses, and wins/losses/ties.
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+        userChoiceText.textContent = "Your guesses so far: " + userChoice;
+      }
+    };
